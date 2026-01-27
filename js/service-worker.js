6d1986effc8af1f912f935088fd39eb68da634a0
Gabriel Brandão <gabriel.baptista.brandao@gmail.com>
2026-01-27 19:25:16 -0300

    Atualiza RAT B.I.T

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,17 +1,43 @@
-const CACHE_NAME = "relatorio-tecnico-v1";
+const CACHE_NAME = "relatorio-tecnico-v2";
+
 const FILES_TO_CACHE = [
   "./",
-  "./index.html"
+  "./index.html",
+  "./css/estilo.css",
+  "./js/relatorio.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
